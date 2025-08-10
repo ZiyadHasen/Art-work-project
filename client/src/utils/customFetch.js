f2f2107d9d05e89167ae850e0fad8ee3386fe754
Ziyad Hasen <Hziyad933@gmail.com>
2025-08-10 13:07:47 +0300

    Remove debug logging from customFetch.js to clean up code and reduce console output.

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const API_HOST = import.meta.env.VITE_API_URL || 'http://localhost:5500';
-console.log('[DEBUG] API_HOST=', API_HOST);
+// console.log('[DEBUG] API_HOST=', API_HOST);
 const baseURL = `${API_HOST.replace(/\/$/, '')}/api/v1`;
 
-console.log('[DEBUG] VITE_API_URL=', import.meta.env.VITE_API_URL);
-console.log('[DEBUG] axios baseURL=', baseURL);
+// console.log('[DEBUG] VITE_API_URL=', import.meta.env.VITE_API_URL);
+// console.log('[DEBUG] axios baseURL=', baseURL);
 
 const customFetch = axios.create({
   baseURL,
