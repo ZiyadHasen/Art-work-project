0795a52f6e7518f7fdaa78b7250ecd89fa095900
Ziyad Hasen <Hziyad933@gmail.com>
2025-08-13 20:00:31 +0300

    tost fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <ToastContainer position='top-left' />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
